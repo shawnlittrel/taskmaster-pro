f97537667dd4118710a1f40edf63891622b846b7
Shawn Littrel <shawnlittrel@protonmail.com>
2021-02-24 18:08:47 -0700

    added drag to trash functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,11 +71,9 @@ $(".list-group").on("blur", "textarea", function () {
 });
 
 //due date was clicked
-$(".list-group").on("click", "span", function(){
+$(".list-group").on("click", "span", function () {
   //get current text
-  var date = $(this)
-    .text()
-    .trim();
+  var date = $(this).text().trim();
 
   //create new input element
   var dateInput = $("<input>")
@@ -91,22 +89,15 @@ $(".list-group").on("click", "span", function(){
 });
 
 //value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("blur", "input[type='text']", function () {
   //get current text
-  var date = $(this)
-    .val()
-    .trim();
+  var date = $(this).val().trim();
 
   //get the parent's ul attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 
   //get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+  var index = $(this).closest(".list-group-item").index();
 
   //update task in array and re-save to localStorage
   tasks[status][index].date = date;
@@ -166,3 +157,65 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+
+//Drag and drop functionality
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    //array to store the task data
+    var tempArr = [];
+    //loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+
+        var date = $(this).find("span").text().trim();
+
+        //add task data to temp array as an object
+        tempArr.push({
+          text: text,
+          date: date,
+        });
+
+      });
+              //trim down list's ID to match object property
+              var arrName = $(this).attr("id").replace("list-", "");
+
+              //update array on tasks object and save
+              tasks[arrName] = tempArr;
+              saveTasks();
+    console.log(tempArr);
+  }
+});
+
+//Enable drag to delete area
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "pointer",
+  drop: function(event,ui){
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event,ui){
+    console.log("out");
+  }
+});
